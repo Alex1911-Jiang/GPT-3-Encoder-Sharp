eafab59d4d9365427a4857c36346e13e687fb399
Alex1911-Jiang <tjxx_jgd@163.com>
2022-12-11 22:40:09 +0800

    Add Files

---
+++ GPT-3-Encoder-Sharp-Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace GPT_3_Encoder_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            var encoder = Encoder.Get_Encoder();
+            string str = 
+                """
+                Many words map to one token, but some don't: indivisible.
+                Unicode characters like emojis may be split into many tokens containing the underlying bytes: 🤚🏾
+                Sequences of characters commonly found next to each other may be grouped together: 1234567890
+                """;
+            var encoded = encoder.Encode(str);
+            Console.WriteLine("base text is: \r\n" + str);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("encoded is: \r\n" + string.Join(',', encoded));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Tokens: {encoded.Count()}, Characters: {str.Length}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("decoded is: \r\n" + encoder.Decode(encoded));
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # GPT-3-Encoder-Sharp
+
 CSharp BPE Encoder Decoder for GPT-3
+
+GPT-3 编码器/解码器的 C# 实现
+
+## About    关于
+
+GPT-3 use byte pair encoding to turn text into a series of integers to feed into the model. This is a C# implementation of OpenAI's original encoder/decoder.
+
+这是一个 OpenAI 原始编码器/解码器的 C# 实现
+
+## Install with Nuget    从 Nuget 安装
+
+```
+Install-Package GPT-3-Encoder-Sharp
+```
+
+## Usage    如何使用
+
+Compatible with .Net >= 6.0
+
+框架要求 >= .Net 6.0
+
+```C#
+            Encoder encoder = Encoder.Get_Encoder();
+            string str = "This is an example sentence to try encoding out on!";
+            var encoded = encoder.Encode(str);
+            Console.WriteLine("encoded is: \r\n" + string.Join(',', encoded));
+            Console.WriteLine($"Tokens: {encoded.Count()}, Characters: {str.Length}");
+            Console.WriteLine("decoded is: \r\n" + encoder.Decode(encoded));
+```
